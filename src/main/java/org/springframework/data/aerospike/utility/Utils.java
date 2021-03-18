a6d137915a9f7ae4efd72ffb6c8798c78d83af25
roimenashe <roimenashe5@gmail.com>
2021-03-18 16:02:13 +0200

    Replace AerospikeClient with IAerospikeClient (excluding cache related classes\tests).

--- src/main/java/org/springframework/data/aerospike/utility/Utils.java
+++ src/main/java/org/springframework/data/aerospike/utility/Utils.java
@@ -15,7 +15,7 @@
  */
 package org.springframework.data.aerospike.utility;
 
-import com.aerospike.client.AerospikeClient;
+import com.aerospike.client.IAerospikeClient;
 import com.aerospike.client.Info;
 import com.aerospike.client.cluster.Node;
 import lombok.experimental.UtilityClass;
@@ -35,8 +35,8 @@ public class Utils {
 	 * @param infoString
 	 * @return
 	 */
-	public static String[] infoAll(AerospikeClient client,
-			String infoString) {
+	public static String[] infoAll(IAerospikeClient client,
+								   String infoString) {
 		String[] messages = new String[client.getNodes().length];
 		int index = 0;
 		for (Node node : client.getNodes()){
