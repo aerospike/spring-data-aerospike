2dfd36c1d5259487160167275532d5d1f6c9941c
roimenashe <roimenashe5@gmail.com>
2022-10-16 11:01:17 +0300

    Format.

--- src/test/java/org/springframework/data/aerospike/sample/PersonRepository.java
+++ src/test/java/org/springframework/data/aerospike/sample/PersonRepository.java
@@ -15,7 +15,6 @@
  */
 package org.springframework.data.aerospike.sample;
 
-import org.springframework.data.aerospike.query.FilterOperation;
 import org.springframework.data.aerospike.repository.AerospikeRepository;
 import org.springframework.data.aerospike.repository.query.CriteriaDefinition;
 import org.springframework.data.domain.Page;
@@ -36,183 +35,190 @@ import java.util.stream.Stream;
  */
 public interface PersonRepository<P extends Person> extends AerospikeRepository<P, String> {
 
-	List<P> findByLastName(String lastName);
+    List<P> findByLastName(String lastName);
 
-	// DTO Projection
-	List<PersonSomeFields> findPersonSomeFieldsByLastName(String lastName);
+    // DTO Projection
+    List<PersonSomeFields> findPersonSomeFieldsByLastName(String lastName);
 
-	// Dynamic Projection
-	<T> List<T> findByLastName(String lastName, Class<T> type);
-	
-	Page<P> findByLastNameStartsWithOrderByAgeAsc(String prefix, Pageable pageable);
+    // Dynamic Projection
+    <T> List<T> findByLastName(String lastName, Class<T> type);
 
-	List<P> findByLastNameEndsWith(String postfix);
+    Page<P> findByLastNameStartsWithOrderByAgeAsc(String prefix, Pageable pageable);
 
-	List<P> findByLastNameOrderByFirstNameAsc(String lastName);
-	
-	List<P> findByLastNameOrderByFirstNameDesc(String lastName);
+    List<P> findByLastNameEndsWith(String postfix);
 
-	List<P> findByFirstNameLike(String firstName);
+    List<P> findByLastNameOrderByFirstNameAsc(String lastName);
 
-	List<P> findByFirstNameLikeOrderByLastNameAsc(String firstName, Sort sort);
+    List<P> findByLastNameOrderByFirstNameDesc(String lastName);
 
-	List<P> findByAgeLessThan(int age, Sort sort);
+    List<P> findByFirstNameLike(String firstName);
 
-	Stream<P> findByFirstNameIn(List<String> firstNames);
+    List<P> findByFirstNameLikeOrderByLastNameAsc(String firstName, Sort sort);
 
-	Stream<P> findByFirstNameNotIn(Collection<String> firstNames);
+    List<P> findByAgeLessThan(int age, Sort sort);
 
-	List<P> findByFirstNameAndLastName(String firstName, String lastName);
+    Stream<P> findByFirstNameIn(List<String> firstNames);
 
-	List<P> findByAgeBetween(int from, int to);
+    Stream<P> findByFirstNameNotIn(Collection<String> firstNames);
 
-	List<P> findByFriendAgeBetween(int from, int to);
+    List<P> findByFirstNameAndLastName(String firstName, String lastName);
 
-	@SuppressWarnings("rawtypes")
-	Person findByShippingAddresses(Set address);
+    List<P> findByAgeBetween(int from, int to);
 
-	List<P> findByAddress(Address address);
+    List<P> findByFriendAgeBetween(int from, int to);
 
-	List<P> findByAddressZipCode(String zipCode);
+    @SuppressWarnings("rawtypes")
+    Person findByShippingAddresses(Set address);
 
-	List<P> findByAddressZipCodeContaining(String str);
+    List<P> findByAddress(Address address);
 
-	List<P> findByLastNameLikeAndAgeBetween(String lastName, int from, int to);
+    List<P> findByAddressZipCode(String zipCode);
 
-	List<P> findByAgeOrLastNameLikeAndFirstNameLike(int age, String lastName, String firstName);
+    List<P> findByAddressZipCodeContaining(String str);
+
+    List<P> findByLastNameLikeAndAgeBetween(String lastName, int from, int to);
+
+    List<P> findByAgeOrLastNameLikeAndFirstNameLike(int age, String lastName, String firstName);
 
 //	List<P> findByNamedQuery(String firstName);
 
-	List<P> findByCreator(User user);
+    List<P> findByCreator(User user);
 
-	List<P> findByCreatedAtLessThan(Date date);
+    List<P> findByCreatedAtLessThan(Date date);
 
-	List<P> findByCreatedAtGreaterThan(Date date);
+    List<P> findByCreatedAtGreaterThan(Date date);
 
 //	List<P> findByCreatedAtLessThanManually(Date date);
 
-	List<P> findByCreatedAtBefore(Date date);
+    List<P> findByCreatedAtBefore(Date date);
 
-	List<P> findByCreatedAtAfter(Date date);
+    List<P> findByCreatedAtAfter(Date date);
 
-	Stream<P> findByLastNameNot(String lastName);
+    Stream<P> findByLastNameNot(String lastName);
 
-	List<P> findByCredentials(Credentials credentials);
-	
-	List<P> findCustomerByAgeBetween(Integer from, Integer to);
+    List<P> findByCredentials(Credentials credentials);
 
-	List<P> findByAgeIn(ArrayList<Integer> ages);
+    List<P> findCustomerByAgeBetween(Integer from, Integer to);
 
-	List<P> findPersonByFirstName(String firstName);
+    List<P> findByAgeIn(ArrayList<Integer> ages);
 
-	long countByLastName(String lastName);
+    List<P> findPersonByFirstName(String firstName);
 
-	int countByFirstName(String firstName);
+    long countByLastName(String lastName);
 
-	long someCountQuery(String lastName);
+    int countByFirstName(String firstName);
 
-	List<P> findByFirstNameIgnoreCase(String firstName);
+    long someCountQuery(String lastName);
 
-	List<P> findByFirstNameNotIgnoreCase(String firstName);
+    List<P> findByFirstNameIgnoreCase(String firstName);
 
-	List<P> findByFirstNameStartingWithIgnoreCase(String firstName);
+    List<P> findByFirstNameNotIgnoreCase(String firstName);
 
-	List<P> findByFirstNameEndingWithIgnoreCase(String firstName);
+    List<P> findByFirstNameStartingWithIgnoreCase(String firstName);
 
-	List<P> findByFirstNameContainingIgnoreCase(String firstName);
+    List<P> findByFirstNameEndingWithIgnoreCase(String firstName);
 
-	Slice<P> findByAgeGreaterThan(int age, Pageable pageable);
+    List<P> findByFirstNameContainingIgnoreCase(String firstName);
 
-	// DTO Projection
-	Slice<PersonSomeFields> findPersonSomeFieldsByAgeGreaterThan(int age, Pageable pageable);
+    Slice<P> findByAgeGreaterThan(int age, Pageable pageable);
 
-	List<P> deleteByLastName(String lastName);
+    // DTO Projection
+    Slice<PersonSomeFields> findPersonSomeFieldsByAgeGreaterThan(int age, Pageable pageable);
 
-	Long deletePersonByLastName(String lastName);
+    List<P> deleteByLastName(String lastName);
 
-	Page<P> findByAddressIn(List<Address> address, Pageable page);
+    Long deletePersonByLastName(String lastName);
 
-	/**
-	 * Find all entities containing the given map element (key or value depending on the given criteria)
-	 * @param element  map element
-	 * @param criteria  KEY or VALUE
-	 */
-	List<P> findByStringMapContaining(String element, CriteriaDefinition.AerospikeMapCriteria criteria);
+    Page<P> findByAddressIn(List<Address> address, Pageable page);
 
-	/**
-	 * Find all entities that satisfy the condition "have exactly the given map key and the given value"
-	 * @param key Map key
-	 * @param value Value of the key
-	 */
-	List<P> findByStringMapEquals(String key, String value);
+    /**
+     * Find all entities containing the given map element (key or value depending on the given criteria)
+     *
+     * @param element  map element
+     * @param criteria KEY or VALUE
+     */
+    List<P> findByStringMapContaining(String element, CriteriaDefinition.AerospikeMapCriteria criteria);
 
-	/**
-	 * Find all entities that satisfy the condition "have the given map key and NOT the given value"
-	 * @param key Map key
-	 * @param value Value of the key
-	 */
-	List<P> findByIntMapIsNot(String key, Integer value);
+    /**
+     * Find all entities that satisfy the condition "have exactly the given map key and the given value"
+     *
+     * @param key   Map key
+     * @param value Value of the key
+     */
+    List<P> findByStringMapEquals(String key, String value);
 
-	/**
-	 * Find all entities that satisfy the condition "have the given map key and a value that starts with the given string"
-	 * @param key Map key
-	 * @param valueStartsWith String to check if value starts with it
-	 */
-	List<P> findByStringMapStartsWith(String key, String valueStartsWith);
+    /**
+     * Find all entities that satisfy the condition "have the given map key and NOT the given value"
+     *
+     * @param key   Map key
+     * @param value Value of the key
+     */
+    List<P> findByIntMapIsNot(String key, Integer value);
 
-	/**
-	 * Find all entities that satisfy the condition "have the given map key and a value that contains the given string"
-	 * @param key Map key
-	 * @param valuePart String to check if value contains it
-	 */
-	List<P> findByStringMapContaining(String key, String valuePart);
+    /**
+     * Find all entities that satisfy the condition "have the given map key and a value that starts with the given string"
+     *
+     * @param key             Map key
+     * @param valueStartsWith String to check if value starts with it
+     */
+    List<P> findByStringMapStartsWith(String key, String valueStartsWith);
 
-	/**
-	 * Find all entities that satisfy the condition "have the given map key and a value that is greater than the given integer"
-	 * @param key Map key
-	 * @param greaterThan Integer to check if value is greater than it
-	 */
-	List<P> findByIntMapGreaterThan(String key, Integer greaterThan);
+    /**
+     * Find all entities that satisfy the condition "have the given map key and a value that contains the given string"
+     *
+     * @param key       Map key
+     * @param valuePart String to check if value contains it
+     */
+    List<P> findByStringMapContaining(String key, String valuePart);
 
-	/**
-	 * Find all entities that satisfy the condition "have the given map key and a value in between the given integers"
-	 * @param key Map key
-	 * @param from the lower limit for the map value, inclusive
-	 * @param to the upper limit for the map value, inclusive
-	 */
-	List<P> findByIntMapBetween(String key, Integer from, Integer to);
+    /**
+     * Find all entities that satisfy the condition "have the given map key and a value that is greater than the given integer"
+     *
+     * @param key         Map key
+     * @param greaterThan Integer to check if value is greater than it
+     */
+    List<P> findByIntMapGreaterThan(String key, Integer greaterThan);
 
-	List<P> findByFriendLastName(String value);
+    /**
+     * Find all entities that satisfy the condition "have the given map key and a value in between the given integers"
+     *
+     * @param key  Map key
+     * @param from the lower limit for the map value, inclusive
+     * @param to   the upper limit for the map value, inclusive
+     */
+    List<P> findByIntMapBetween(String key, Integer from, Integer to);
 
-	List<P> findByFriendAge(int value);
+    List<P> findByFriendLastName(String value);
 
-	List<P> findByFriendAgeGreaterThan(int value);
+    List<P> findByFriendAge(int value);
 
-	List<P> findByFriendAgeGreaterThanEqual(int value);
+    List<P> findByFriendAgeGreaterThan(int value);
 
-	List<P> findByStringsContaining(String string);
+    List<P> findByFriendAgeGreaterThanEqual(int value);
 
-	List<P> findByIntsContaining(Integer integer);
+    List<P> findByStringsContaining(String string);
 
-	List<P> findByIntsGreaterThan(Integer integer);
+    List<P> findByIntsContaining(Integer integer);
 
-	List<P> findByIntsLessThanEqual(Integer integer);
+    List<P> findByIntsGreaterThan(Integer integer);
 
-	List<P> findByIntsContaining(List<Integer> integer);
+    List<P> findByIntsLessThanEqual(Integer integer);
 
-	List<P> findTop3ByLastNameStartingWith(String lastName);
+    List<P> findByIntsContaining(List<Integer> integer);
 
-	Page<P> findTop3ByLastNameStartingWith(String lastName, Pageable pageRequest);
+    List<P> findTop3ByLastNameStartingWith(String lastName);
 
-	List<P> findByFirstName(String string);
+    Page<P> findTop3ByLastNameStartingWith(String lastName, Pageable pageRequest);
 
-	List<P> findByFirstNameAndAge(String string, int i);
+    List<P> findByFirstName(String string);
 
-	Iterable<P> findByAgeBetweenAndLastName(int from, int to, String lastName);
+    List<P> findByFirstNameAndAge(String string, int i);
 
-	List<P> findByFirstNameStartsWith(String string);
+    Iterable<P> findByAgeBetweenAndLastName(int from, int to, String lastName);
 
-	List<P> findByFriendFirstNameStartsWith(String string);
+    List<P> findByFirstNameStartsWith(String string);
 
-	Iterable<P> findByAgeBetweenOrderByLastName(int i, int j);
+    List<P> findByFriendFirstNameStartsWith(String string);
+
+    Iterable<P> findByAgeBetweenOrderByLastName(int i, int j);
 }
