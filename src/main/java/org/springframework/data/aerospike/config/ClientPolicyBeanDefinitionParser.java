3f6c6e9021eae7cc8bd351a5eedbbe96b87ce119
agrgr <agrgr@users.noreply.github.com>
2023-05-11 11:53:00 +0300

    TODOs cleanup

--- src/main/java/org/springframework/data/aerospike/config/ClientPolicyBeanDefinitionParser.java
+++ src/main/java/org/springframework/data/aerospike/config/ClientPolicyBeanDefinitionParser.java
@@ -39,6 +39,5 @@ public class ClientPolicyBeanDefinitionParser extends AbstractSingleBeanDefiniti
     protected void doParse(Element element, BeanDefinitionBuilder builder) {
         ParsingUtils.setPropertyValue(builder, element, "maxThreads");
         ParsingUtils.setPropertyValue(builder, element, "timeOut");
-        // TODO: set all properties of the XML element on the builder
     }
 }
