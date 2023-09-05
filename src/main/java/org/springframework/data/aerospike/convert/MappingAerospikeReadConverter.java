4144e6f08ed6eb2ee5c4ecda427cc57b21403ea4
agrgr <agrgr@users.noreply.github.com>
2023-09-05 15:41:06 +0300

    update testcontainers to 3.0.0

--- src/main/java/org/springframework/data/aerospike/convert/MappingAerospikeReadConverter.java
+++ src/main/java/org/springframework/data/aerospike/convert/MappingAerospikeReadConverter.java
@@ -181,9 +181,15 @@ public class MappingAerospikeReadConverter implements EntityReader<Object, Aeros
 
         Map<Object, Object> converted = CollectionFactory.createMap(mapClass, keyClass, source.keySet().size());
 
-        source.forEach((k, v) -> {
-            Object key = keyClass != null ? conversionService.convert(k, keyClass) : k;
-            Object value = readValue(v, mapValueType);
+//        source.forEach((k, v) -> {
+//            Object key = keyClass != null ? conversionService.convert(k, keyClass) : k;
+//            Object value = readValue(v, mapValueType);
+//            converted.put(key, value);
+//        });
+
+        source.entrySet().forEach((e) -> {
+            Object key = keyClass != null ? conversionService.convert(e.getKey(), keyClass) : e.getKey();
+            Object value = readValue(e.getValue(), mapValueType);
             converted.put(key, value);
         });
 
