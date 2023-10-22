3890aea1849e056e6c0c1bedc02e58ca694446f7
agrgr <agrgr@users.noreply.github.com>
2023-10-22 14:34:29 +0300

    minor rephrase for javadoc

--- src/main/java/org/springframework/data/aerospike/query/Qualifier.java
+++ src/main/java/org/springframework/data/aerospike/query/Qualifier.java
@@ -381,7 +381,7 @@ public class Qualifier implements Map<String, Object>, Serializable {
     }
 
     /**
-     * Create Qualifier with the condition "Id equal to the given String"
+     * Create qualifier "ID equal to the given string"
      *
      * @param id String value
      * @return Single id qualifier
@@ -394,7 +394,7 @@ public class Qualifier implements Map<String, Object>, Serializable {
     }
 
     /**
-     * Create Qualifier with the condition "Id equal to one of the given Strings (logical OR)"
+     * Create qualifier "ID equal to one of the given strings (logical OR)"
      *
      * @param ids String values
      * @return Multiple ids qualifier with OR condition
