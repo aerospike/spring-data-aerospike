85d60221bdfef9232c2222b26f12edfcb6ae7b79
agrgr <agrgr@users.noreply.github.com>
2024-05-16 13:44:34 +0300

    cleanup

--- src/test/java/org/springframework/data/aerospike/core/AerospikeTemplateFindByQueryTests.java
+++ src/test/java/org/springframework/data/aerospike/core/AerospikeTemplateFindByQueryTests.java
@@ -84,6 +84,7 @@ public class AerospikeTemplateFindByQueryTests extends BaseBlockingIntegrationTe
     @BeforeAll
     public void beforeAllSetUp() {
         deleteOneByOne(allPersons);
+        deleteOneByOne(allPersons, OVERRIDE_SET_NAME);
 
         // batch write operations are supported starting with Server version 6.0+
         if (serverVersionSupport.isBatchWriteSupported()) {
