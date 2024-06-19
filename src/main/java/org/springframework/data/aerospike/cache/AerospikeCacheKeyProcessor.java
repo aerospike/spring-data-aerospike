d65c93fc3c89c65e6f89ab16c9410438d47ba45b
agrgr <agrgr@users.noreply.github.com>
2024-06-19 17:00:04 +0300

    convert AerospikeCacheKeyProcessor into an interface

--- src/main/java/org/springframework/data/aerospike/cache/AerospikeCacheKeyProcessor.java
+++ src/main/java/org/springframework/data/aerospike/cache/AerospikeCacheKeyProcessor.java
@@ -1,16 +1,6 @@
 package org.springframework.data.aerospike.cache;
 
-import com.esotericsoftware.kryo.Kryo;
-import com.esotericsoftware.kryo.io.ByteBufferOutput;
-import lombok.Getter;
-import org.apache.commons.codec.digest.MurmurHash3;
-
-import java.util.Arrays;
-
-public abstract class AerospikeCacheKeyProcessor {
-
-    @Getter
-    private static final Kryo kryoInstance = new Kryo();
+public interface AerospikeCacheKeyProcessor {
 
     /**
      * Serialize the given key and calculate hash based on the serialization result.
@@ -18,9 +8,7 @@ public abstract class AerospikeCacheKeyProcessor {
      * @param key Object to be serialized and hashed
      * @return AerospikeCacheKey instantiated with either a String or a long number
      */
-    public AerospikeCacheKey serializeAndHash(Object key) {
-        return calculateHash(serialize(key));
-    }
+    AerospikeCacheKey serializeAndHash(Object key);
 
     /**
      * Serialize the given key.
@@ -30,12 +18,7 @@ public abstract class AerospikeCacheKeyProcessor {
      * @param key Object to be serialized
      * @return byte[]
      */
-    public byte[] serialize(Object key) {
-        ByteBufferOutput output = new ByteBufferOutput(1024); // Initial buffer size
-        kryoInstance.writeClassAndObject(output, key);
-        output.flush();
-        return output.toBytes();
-    }
+    byte[] serialize(Object key);
 
     /**
      * Calculate hash based on the given byte array.
@@ -45,7 +28,5 @@ public abstract class AerospikeCacheKeyProcessor {
      * @param data Byte array to be hashed
      * @return AerospikeCacheKey instantiated with either a String or a long number
      */
-    public AerospikeCacheKey calculateHash(byte[] data) {
-        return AerospikeCacheKey.of(Arrays.toString(MurmurHash3.hash128(data)));
-    }
+    AerospikeCacheKey calculateHash(byte[] data);
 }
--- src/main/java/org/springframework/data/aerospike/cache/AerospikeCacheKeyProcessorImpl.java
+++ src/main/java/org/springframework/data/aerospike/cache/AerospikeCacheKeyProcessorImpl.java
@@ -1,8 +1,17 @@
 package org.springframework.data.aerospike.cache;
 
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.ByteBufferOutput;
+import lombok.Getter;
+import org.apache.commons.codec.digest.MurmurHash3;
 import org.objenesis.strategy.StdInstantiatorStrategy;
 
-public class AerospikeCacheKeyProcessorImpl extends AerospikeCacheKeyProcessor {
+import java.util.Arrays;
+
+public class AerospikeCacheKeyProcessorImpl implements AerospikeCacheKeyProcessor {
+
+    @Getter
+    private static final Kryo kryoInstance = new Kryo();
 
     public AerospikeCacheKeyProcessorImpl() {
         configureKryo();
@@ -19,5 +28,20 @@ public class AerospikeCacheKeyProcessorImpl extends AerospikeCacheKeyProcessor {
         // setting to false means not requiring registration for all the classes of cached objects in advance
         getKryoInstance().setRegistrationRequired(false);
         getKryoInstance().setInstantiatorStrategy(new StdInstantiatorStrategy());
+    }
+
+    public AerospikeCacheKey serializeAndHash(Object key) {
+        return calculateHash(serialize(key));
+    }
+
+    public byte[] serialize(Object key) {
+        ByteBufferOutput output = new ByteBufferOutput(1024); // Initial buffer size
+        kryoInstance.writeClassAndObject(output, key);
+        output.flush();
+        return output.toBytes();
+    }
+
+    public AerospikeCacheKey calculateHash(byte[] data) {
+        return AerospikeCacheKey.of(Arrays.toString(MurmurHash3.hash128(data)));
     }
 }
