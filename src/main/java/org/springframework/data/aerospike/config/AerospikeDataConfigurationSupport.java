e20da622d559a01f5f294195af6ab24ff68a02ba
Gagan Mishra <gmishra@aerospike.com>
2025-11-24 15:34:43 -0800

    add auth properties

--- src/main/java/org/springframework/data/aerospike/config/AerospikeDataConfigurationSupport.java
+++ src/main/java/org/springframework/data/aerospike/config/AerospikeDataConfigurationSupport.java
@@ -36,6 +36,7 @@ import org.springframework.beans.BeanInstantiationException;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
 import org.springframework.context.annotation.Configuration;
@@ -77,11 +78,18 @@ import java.util.Set;
  */
 @Slf4j
 @Configuration
-public abstract class AerospikeDataConfigurationSupport {
+public abstract class AerospikeDataConfigurationSupport implements EnvironmentAware {
 
     public static final String CONFIG_PREFIX = "spring";
     public static final String CONFIG_PREFIX_DATA = CONFIG_PREFIX + ".data.aerospike";
     public static final String CONFIG_PREFIX_CONNECTION = CONFIG_PREFIX + ".aerospike";
+
+    protected Environment environment;
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
+    }
 
     @Bean(name = "aerospikeQueryContextBuilder")
     public QueryContextBuilder queryContextBuilder(IndexesCache indexesCache) {
@@ -293,6 +301,10 @@ public abstract class AerospikeDataConfigurationSupport {
         clientPolicy.queryPolicyDefault.sendKey = sendKey;
         clientPolicy.scanPolicyDefault.sendKey = sendKey;
         clientPolicy.eventLoops = eventLoops();
+
+        AerospikeConnectionSettings aerospikeConnectionSettings = readAerospikeSettings(this.environment);
+        clientPolicy.setUser(aerospikeConnectionSettings.getUser());
+        clientPolicy.setPassword(aerospikeConnectionSettings.getPassword());
         return clientPolicy;
     }
 
