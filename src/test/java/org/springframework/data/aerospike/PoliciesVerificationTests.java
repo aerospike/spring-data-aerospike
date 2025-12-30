91ed32611d262263bc9cf4e2bb864b70c72aa37d
romanstepanenkov <roman.stepanenkov@gmail.com>
2025-12-30 13:46:02 +0100

    test `sendKey` policies

--- src/test/java/org/springframework/data/aerospike/PoliciesVerificationTests.java
+++ src/test/java/org/springframework/data/aerospike/PoliciesVerificationTests.java
@@ -9,5 +9,11 @@ public class PoliciesVerificationTests extends BaseBlockingIntegrationTests {
     @Test
     public void sendKeyShouldBeTrueByDefault() {
         assertThat(client.getWritePolicyDefault().sendKey).isTrue();
+        assertThat(client.getReadPolicyDefault().sendKey).isTrue();
+        assertThat(client.getBatchPolicyDefault().sendKey).isTrue();
+        assertThat(client.getBatchWritePolicyDefault().sendKey).isTrue();
+        assertThat(client.getBatchDeletePolicyDefault().sendKey).isTrue();
+        assertThat(client.getQueryPolicyDefault().sendKey).isTrue();
+        assertThat(client.getScanPolicyDefault().sendKey).isTrue();
     }
 }
